6df241da8d87ab6fd2f637c5299e6c2358273722
Lance McCarthy <lance.mccarthy@telerik.com>
2016-01-27 17:41:07 -0500

    Added IsVisible AttachedProperty helper class
    Added the attached property XamlHelper.IsVisible. It allows bool value
    to toggle a UIElement's Visibility. This eliminates the need for a
    ValueConverte for simple bool > Visibility bindings

---
+++ UwpHelpers/UwpHelpers.Controls/Extensions/XamlExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+
+namespace UwpHelpers.Controls.Extensions
+{
+    /// <summary>
+    /// Attachable properties to use instead of common ValueConverters
+    /// </summary>
+    public class XamlExtensions : DependencyObject
+    {
+        /// <summary>
+        /// Use a bool to trigger UIElement's Visibility value (instead of using a ValueConverter)
+        /// Usage: extensions:XamlExtensions.IsVisible="True"
+        /// </summary>
+        public static readonly DependencyProperty IsVisibleProperty = DependencyProperty.RegisterAttached(
+            "IsVisible", typeof (bool), typeof (XamlExtensions), new PropertyMetadata(true, (o,e) =>
+            {
+                ((UIElement)o).Visibility = (bool)e.NewValue ? Visibility.Visible:Visibility.Collapsed;
+            }));
+
+        public static void SetIsVisible(DependencyObject element, bool value)
+        {
+            element.SetValue(IsVisibleProperty, value);
+        }
+
+        public static bool GetIsVisible(DependencyObject element) => (bool) element.GetValue(IsVisibleProperty);
+    }
+}
