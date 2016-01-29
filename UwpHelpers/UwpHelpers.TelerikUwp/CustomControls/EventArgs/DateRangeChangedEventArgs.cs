41a02303db51a503e40b7441cddca7ab2d5e0439
Lance McCarthy <lance.mccarthy@telerik.com>
2016-01-29 13:59:25 -0500

    Added custom DateRangePicker to new TelerikUWP project
    Created a DateRangePicker control using Telerik UI for UWP
    RadDatePickers. I put this control in a new TelerikUwp project so that
    there would not be any Telerik dependencies in the normal UwpHelpers
    project

---
+++ UwpHelpers/UwpHelpers.TelerikUwp/CustomControls/EventArgs/DateRangeChangedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UwpHelpers.TelerikUwp.CustomControls.EventArgs
+{
+    public class DateRangeChangedEventArgs : System.EventArgs
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public DateRangeChangedEventArgs(DateTime startDate, DateTime endDate)
+        {
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+    }
+}
---
+++ UwpHelpers/UwpHelpers.TelerikUwp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml.Controls;
+using UwpHelpers.TelerikUwp.CustomControls.EventArgs;
+
+namespace UwpHelpers.TelerikUwp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MyDateRangePicker_OnDateRangeValueChanged(object sender, DateRangeChangedEventArgs e)
+        {
+            StartDateTextBlock.Text = e.StartDate.ToString("MM/dd/yyyy hh:mm tt");
+            EndDateTextBlock.Text = e.EndDate.ToString("MM/dd/yyyy hh:mm tt");
+        }
+    }
+}
